7bd8c106198b399048e9aa2a659798a9de47ed93
Ashley Abbott <ashley.abbott@eagleeye.com>
2019-05-22 17:27:53 +0100

    simplified things

--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -11,40 +11,14 @@ require 'chef/version_constraint'
 
 include_recipe 'libarchive::default' unless Chef::VERSION.to_i >= 14
 
-url = ::URI.join(node['consul_template']['base_url'],
-                 "#{node['consul_template']['version']}/",
-                 ConsulTemplateHelpers.install_file(node)).to_s
-
-download_path = "#{Chef::Config['file_cache_path']}/#{ConsulTemplateHelpers.install_file(node)}"
-install_path = "#{node['consul_template']['install_dir']}/#{ConsulTemplateHelpers.install_version(node)}"
-
-remote_file download_path do
-  source url
+remote_file "#{Chef::Config[:file_cache_path]}/consul-template-#{node['consul_template']['version']}.zip" do
+  source "https://releases.hashicorp.com/consul-template/#{node['consul_template']['version']}/consul-template_#{node['consul_template']['version']}_linux_amd64.zip"
   checksum ConsulTemplateHelpers.install_checksum(node)
   action :create_if_missing
 end
 
-libarchive_version = run_context.cookbook_collection['libarchive'].version.to_f.round(2)
-case libarchive_version
-when libarchive_version >= 2.0
-  archive_file ConsulTemplateHelpers.install_file(node) do
-    path download_path
-    extract_to install_path
-    mode 0755
-    not_if { File.exist? install_path }
-    action :extract
-  end
-when libarchive_version < 2.0
-  libarchive_file ConsulTemplateHelpers.install_file(node) do
-    path download_path
-    extract_to install_path
-    mode 0755
-    not_if { File.exist? install_path }
-    action :extract
-  end
-end
-
-link "#{node['consul_template']['install_dir']}/consul-template" do
-  to "#{install_path}/consul-template"
-  mode 0755
+bash 'extract consul-template' do
+  code <<~EOH
+    unzip "#{Chef::Config[:file_cache_path]}/consul-template-#{node['consul_template']['version']}.zip" -d /usr/local/bin
+  EOH
 end
