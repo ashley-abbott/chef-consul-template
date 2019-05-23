09dc89c9a4d7876c36e88ce83192b3cf914efd09
Ashley Abbott <ashley.abbott@eagleeye.com>
2019-05-23 14:50:46 +0100

    added a guard to consul-template binary

--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -17,5 +17,5 @@ bash 'extract consul-template' do
   code <<~EOH
     unzip "#{Chef::Config[:file_cache_path]}/consul-template-#{node['consul_template']['version']}.zip" -d /usr/local/bin
   EOH
-  not_if { :File.exist?('/usr/local/bin/consul-template') }
+  not_if { ::File.exist?('/usr/local/bin/consul-template') }
 end
