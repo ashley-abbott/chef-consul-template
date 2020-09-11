8bb57f461f386ab971884690d84365243c2155a6
Ashley Abbott <ashley.abbott@eagleeye.com>
2020-09-11 13:03:17 +0100

    download new version if they differ, new checksums for 0.25.1

--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -17,5 +17,10 @@ bash 'extract consul-template' do
   code <<~EOH
     unzip "#{Chef::Config[:file_cache_path]}/consul-template-#{node['consul_template']['version']}.zip" -d /usr/local/bin
   EOH
-  not_if { ::File.exist?('/usr/local/bin/consul-template') }
+  not_if { Mixlib::ShellOut.new("consul-template --version | grep #{node['consul_template']['version']}").run_command.exitstatus == 0 }
+  if node['consul_template']['init_style'] == 'runit'
+    notifies :restart, 'runit_service[consul-template]', :delayed
+  else
+    notifies :restart, 'service[consul-template]', :delayed
+  end
 end
